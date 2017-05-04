48e719801c7c63387253da66edb3f135b1b2024d
Misa Jokisalo <misa.jokisalo@gmail.com>
2017-05-04 11:39:05 +0300

    Pure JS

--- banner.js
+++ banner.js
@@ -1,4 +1,34 @@
-$.get('content.html').then(function(responseData) {  
-	console.log(responseData);
-	$('<div id="toolbar" style="height: 40px; width: 100%; position: relative; display: block; background-color: #333333; color: #FAFAFA;">' + responseData + '</div>').prependTo('body');
-});
+var url = 'https://misacorp.github.io/cross-site-banner/content.html';
+var content = ajax(url, 'get',  function(obj) { updatePage(obj.responseText) });
+
+updatePage(content);
+
+function updatePage(data) {
+    document.body.innerHTML = data + document.body.innerHTML;
+}
+
+function ajax(url, method, callback, params = null) {
+     var obj;
+     try {   
+      obj = new XMLHttpRequest();  
+     } catch(e){   
+       try {     
+         obj = new ActiveXObject("Msxml2.XMLHTTP");     
+       } catch(e) {     
+         try { 
+           obj = new ActiveXObject("Microsoft.XMLHTTP");       
+         } catch(e) {       
+           alert("Your browser does not support Ajax.");       
+           return false;       
+         }     
+       }   
+     }
+     obj.onreadystatechange = function() {
+      if(obj.readyState == 4) {
+         callback(obj);
+      } 
+     }
+     obj.open(method, url, true);
+     obj.send(params);
+     return obj;
+ }
